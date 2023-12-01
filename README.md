d73b72fdb0c599674ee2063301c0aa39c64228e0
arvindk459895 <arvindk459895@gmail.com>
2023-12-01 15:51:30 +0530

    data ingestion adn notebook added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Text-Summarization-Nlp
+
+
+## Workflow
+
+# Update the config.yaml
+# Update the params.yaml
+# Update the entity
+# Update the configuration manager in src config
+# Update the component
+# Update the pipeline
+# Update the main.py
+# Update the app.py
+
+# Dataset :- https://github.com/entbappy/Branching-tutorial/raw/master/samsumdata.zip
--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+from TextSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from TextSummarizer.logging import logger
 
-longer.info("Welcome the custom log")
+STAGE_NAME = "data_ingestion_training"
+try:
+    logger.info(f">>>>>> stage{STAGE_NAME} started<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>> stage{STAGE_NAME} finished<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
